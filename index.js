1e28d1865a2511aed4cd14ec671df761a8319ef1
whoover <william.hoover@nemours.org>
2021-08-17 19:07:09 -0700

    [DOCS]: Cleanup [skip travis]

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 'use strict';
 
+const DBDriver = require('oracledb');
+const Stream = require('stream');
+const EventEmitter = require('events');
+const typedefs = require('sqler/typedefs');
+
 /**
  * Oracle database {@link Dialect} implementation for [`sqler`](https://ugate.github.io/sqler/)
  */
-module.exports = class OracleDialect {
+class OracleDialect {
 
   /**
    * Constructor
    * @constructs OracleDialect
-   * @param {SQLERPrivateOptions} priv The private configuration options
+   * @param {typedefs.SQLERPrivateOptions} priv The private configuration options
    * @param {OracleConnectionOptions} connConf The individual SQL __connection__ configuration for the given dialect that was passed into the originating {@link Manager}
-   * @param {SQLERTrack} track Container for sharing data between {@link Dialect} instances.
+   * @param {typedefs.SQLERTrack} track Container for sharing data between {@link Dialect} instances.
    * @param {Function} [errorLogger] A function that takes one or more arguments and logs the results as an error (similar to `console.error`)
    * @param {Function} [logger] A function that takes one or more arguments and logs the results (similar to `console.log`)
    * @param {Boolean} [debug] A flag that indicates the dialect should be run in debug mode (if supported)
@@ -24,8 +29,7 @@ module.exports = class OracleDialect {
       pending: 0
     };
 
-    dlt.at.driver = require('oracledb');
-    dlt.at.driver.Promise = Promise; // tell Oracle to use the built-in promise
+    dlt.at.driver = DBDriver;
 
     const hasDrvrOpts = !!connConf.driverOptions;
     const dopts = hasDrvrOpts && connConf.driverOptions.global;
@@ -34,6 +38,7 @@ module.exports = class OracleDialect {
     dlt.at.driver.autoCommit = true;
     dlt.at.driver.connectionClass = dlt.at.driver.connectionClass || `SqlerOracleGen${Math.floor(Math.random() * 10000)}`;
 
+    /** @type {DBDriver.PoolAttributes} */
     const poolOpts = connConf.pool || {};
     const alias = poolOpts.alias || `sqlerOracleGen${Math.floor(Math.random() * 10000)}`;
     dlt.at.errorLogger = errorLogger;
@@ -81,12 +86,13 @@ module.exports = class OracleDialect {
 
   /**
    * Initializes {@link OracleDialect} by creating the connection pool
-   * @param {Dialect~DialectInitOptions} opts The options described by the `sqler` module
+   * @param {typedefs.SQLERInitOptions} opts The options described by the `sqler` module
    * @returns {Object} The Oracle connection pool
    */
   async init(opts) {
     const dlt = internal(this), numSql = opts.numOfPreparedFuncs;
     statementCacheSize(dlt, numSql);
+    /** @type {DBDriver.Pool} */
     let oraPool;
     try {
       try {
@@ -127,14 +133,15 @@ module.exports = class OracleDialect {
   /**
    * Begins a transaction by opening a connection from the pool
    * @param {String} txId The transaction ID that will be started
-   * @returns {SQLERTransaction} The transaction
+   * @param {typedefs.SQLERTransactionOptions} opts The transaction options passed in via the public API
+   * @returns {typedefs.SQLERTransaction} The transaction that was started
    */
   async beginTransaction(txId) {
     const dlt = internal(this);
     if (dlt.at.logger) {
       dlt.at.logger(`sqler-oracle: Beginning transaction ${txId} on connection pool "${dlt.at.pool.oracleConf.poolAlias}"`);
     }
-    /** @type {SQLERTransaction} */
+    /** @type {typedefs.SQLERTransaction} */
     const tx = {
       id: txId,
       state: Object.seal({
@@ -146,6 +153,7 @@ module.exports = class OracleDialect {
     const pool = dlt.at.driver.getPool(dlt.at.pool.oracleConf.poolAlias);
     /** @type {OracleTransactionObject} */
     const txo = { tx, conn: await dlt.this.getConnection(pool, dlt.at.connConf) };
+    /** @type {typedefs.SQLERExecOptions} */
     const opts = { transactionId: tx.id };
     tx.commit = operation(dlt, 'commit', true, txo, opts, 'unprepare');
     tx.rollback = operation(dlt, 'rollback', true, txo, opts, 'unprepare');
@@ -159,9 +167,9 @@ module.exports = class OracleDialect {
    * @param {String} sql the SQL to execute
    * @param {OracleExecOptions} opts The execution options
    * @param {String[]} frags the frament keys within the SQL that will be retained
-   * @param {SQLERExecMeta} meta The SQL execution metadata
-   * @param {(SQLERExecErrorOptions | Boolean)} [errorOpts] The error options to use
-   * @returns {SQLERExecResults} The execution results
+   * @param {typedefs.SQLERExecMeta} meta The SQL execution metadata
+   * @param {(typedefs.SQLERExecErrorOptions | Boolean)} [errorOpts] The error options to use
+   * @returns {typedefs.SQLERExecResults} The execution results
    */
   async exec(sql, opts, frags, meta, errorOpts) {
     const dlt = internal(this), numSql = opts.numOfPreparedFuncs;
@@ -169,7 +177,13 @@ module.exports = class OracleDialect {
     const pool = dlt.at.driver.getPool(dlt.at.pool.oracleConf.poolAlias);
     /** @type {OracleTransactionObject} */
     const txo = opts.transactionId ? dlt.at.transactions.get(opts.transactionId) : null;
-    let conn, bndp = {}, dopts, rslts, xopts, error;
+    /** @type {DBDriver.Connection} */
+    let conn;
+    /** @type {OracleExecDriverOptions} */
+    let dopts;
+    /** @type {DBDriver.ExecuteOptions} */
+    let xopts;
+    let bndp = {}, rslts, xopts, error;
     try {
       // interpolate and remove unused binds since
       // Oracle will throw "ORA-01036: illegal variable name/number" when unused bind parameters are passed (also, cuts down on payload bloat)
@@ -182,6 +196,7 @@ module.exports = class OracleDialect {
 
       conn = txo ? null : await dlt.this.getConnection(pool, opts);
 
+      /** @type {typedefs.SQLERExecResults} */
       const rtn = {};
 
       if (opts.prepareStatement) {
@@ -232,13 +247,14 @@ module.exports = class OracleDialect {
   /**
    * Gets a new connection from the pool
    * @protected
-   * @param {Object} pool The connection pool
+   * @param {DBDriver.Pool} pool The connection pool
    * @param {OracleExecOptions} [opts] The execution options
    * @returns {Object} The connection (when present)
    */
   async getConnection(pool, opts) {
     const dlt = internal(this);
     const hasDrvrOpts = opts && !!opts.driverOptions;
+    /** @type {DBDriver.PoolAttributes} */
     const poolAttrs = (hasDrvrOpts && opts.driverOptions.pool) || {};
     poolAttrs.poolAlias = dlt.at.pool.oracleConf.poolAlias;
     return pool.getConnection(poolAttrs);
@@ -246,11 +262,12 @@ module.exports = class OracleDialect {
 
   /**
    * Closes the Oracle connection pool
-   * @returns {Integer} The number of connections closed
+   * @returns {Number} The number of connections closed
    */
   async close() {
     const dlt = internal(this);
     try {
+      /** @type {DBDriver.Pool} */
       let pool;
       try {
         pool = dlt.at.driver.getPool(dlt.at.pool.oracleConf.poolAlias);
@@ -272,10 +289,11 @@ module.exports = class OracleDialect {
   }
 
   /**
-   * @returns {SQLERState} The state
+   * @returns {typedefs.SQLERState} The state
    */
   get state() {
     const dlt = internal(this);
+    /** @type {DBDriver.Pool} */
     let pooled;
     try {
       pooled = dlt.at.driver.getPool(dlt.at.pool.oracleConf.poolAlias);
@@ -293,27 +311,30 @@ module.exports = class OracleDialect {
 
   /**
    * @protected
-   * @returns {Object} The oracledb driver module
+   * @returns {DBDriver} The `oracledb` driver module
    */
   get driver() {
     return internal(this).at.driver;
   }
-};
+}
+
+module.exports = OracleDialect;
 
 /**
  * Executes a function by name that resides on the Oracle connection
  * @private
- * @param {Object} dlt The internal Oracle object instance
+ * @param {InternalOracleDB} dlt The internal Oracle object instance
  * @param {String} name The name of the function that will be called on the connection
  * @param {Boolean} [reset] Truthy to reset the pending connection and transaction count when the operation completes successfully
- * @param {(PGTransactionObject | Object)} txoOrConn Either the transaction object or the connection itself
- * @param {SQLERExecOptions} [opts] The {@link SQLERExecOptions}
+ * @param {(OracleTransactionObject | DBDriver.Connection)} txoOrConn Either the transaction object or the connection itself
+ * @param {typedefs.SQLERExecOptions} [opts] The {@link typedefs.SQLERExecOptions}
  * @returns {Function} A no-arguement `async` function that returns the number or pending transactions
  */
 function operation(dlt, name, reset, txoOrConn, opts) {
   return async () => {
     /** @type {OracleTransactionObject} */
     const txo = opts.transactionId && txoOrConn.tx ? txoOrConn : null;
+    /** @type {DBDriver.Connection} */
     const conn = txo ? txo.conn : txoOrConn;
     let ierr;
     try {
@@ -351,9 +372,9 @@ function operation(dlt, name, reset, txoOrConn, opts) {
  * There is no prepare/unprepare since Oracle uses {@link https://oracle.github.io/node-oracledb/doc/api.html#-313-statement-caching statement caching}.
  * Statement cache should account for the number of prepared functions/SQL files by a factor of `3x` to accomodate that many fragments in each SQL file.
  * @private
- * @param {Object} dlt The internal Oracle object instance
- * @param {Integer} numSql The total number of SQL files used on the dialect
- * @returns {Integer} The statement cache size
+ * @param {InternalOracleDB} dlt The internal Oracle object instance
+ * @param {Number} numSql The total number of SQL files used on the dialect
+ * @returns {Number} The statement cache size
  */
 function statementCacheSize(dlt, numSql) {
   dlt.at.pool.oracleConf.stmtCacheSize = (dlt.at.driverOptions && dlt.at.driverOptions.stmtCacheSize) || ((numSql || 1) * 3);
@@ -362,58 +383,101 @@ function statementCacheSize(dlt, numSql) {
 
 // private mapping
 let map = new WeakMap();
-let internal = function(object) {
-  if (!map.has(object)) {
-    map.set(object, {});
+
+/**
+ * Internal state generator
+ * @private
+ * @param {DBDriver} dialect The dialect driver
+ * @returns {InternalOracleDB} TThe internal dialect state
+ */
+let internal = function(dialect) {
+  if (!map.has(dialect)) {
+    map.set(dialect, {});
   }
   return {
-    at: map.get(object),
-    this: object
+    at: map.get(dialect),
+    this: dialect
   };
 };
 
 /**
- * Oracle specific extension of the {@link SQLERConnectionOptions} from the [`sqler`](https://ugate.github.io/sqler/) module.
- * @typedef {SQLERConnectionOptions} OracleConnectionOptions
- * @property {Object} [driverOptions] The `oracledb` module specific options.
- * @property {Object} [driverOptions.global] An object that will contain properties set on the global `oracledb` module class.
- * When a value is a string surrounded by `${}`, it will be assumed to be a _constant_ property that resides on the `oracledb` module and will be interpolated
- * accordingly.
+ * Oracle specific driver options
+ * @typedef {Object} OracleDriverOptions
+ * @property {Object} [global] An object that will contain properties set on the global `oracledb` module class. When a value is a string surrounded by `${}`,
+ * it will be assumed to be a _constant_ property that resides on the `oracledb` module and will be interpolated accordingly.
  * For example `driverOptions.global.someProp = '${ORACLEDB_CONSTANT}'` will be interpolated as `oracledb.someProp = oracledb.ORACLEDB_CONSTANT`.
- * @property {Object} [driverOptions.pool] The pool `conf` options that will be passed into `oracledb.createPool({ conf })`.
- * __Using any of the generic `pool.someOption` will override the `conf` options set on `driverOptions.pool`.__
- * When a value is a string surrounded by `${}`, it will be assumed to be a _constant_ property that resides on the `oracledb` module and will be interpolated
- * accordingly.
+ * @property {Object} [pool] The pool `conf` options that will be passed into `oracledb.createPool({ conf })`. __Using any of the generic `pool.someOption`
+ * will override the `conf` options set on `driverOptions.pool`.__ When a value is a string surrounded by `${}`, it will be assumed to be a _constant_
+ * property that resides on the `oracledb` module and will be interpolated accordingly.
  * For example `driverOptions.pool.someProp = '${ORACLEDB_CONSTANT}'` will be interpolated as `pool.someProp = oracledb.ORACLEDB_CONSTANT`.
- * @property {Boolean} [driverOptions.pingOnInit=true] A truthy flag that indicates if a _ping_ will be performed after the connection pool is created when
+ * @property {Boolean} [pingOnInit=true] A truthy flag that indicates if a _ping_ will be performed after the connection pool is created when
  * {@link OracleDialect.init} is called.
- * @property {Boolean} [driverOptions.useTNS] Truthy to build a TNS `sql*net` connection string
- * @property {Integer} [driverOptions.stmtCacheSize=numberOfSQLFiles * 3] The statement size that `oracledb` uses
+ * @property {Boolean} [useTNS] Truthy to build a TNS `sql*net` connection string
+ * @property {Number} [stmtCacheSize=numberOfSQLFiles * 3] The statement size that `oracledb` uses
  */
 
 /**
- * Oracle specific extension of the {@link SQLERExecOptions} from the [`sqler`](https://ugate.github.io/sqler/) module. When a property of `binds` contains
- * an object it will be _interpolated_ for property values on the `oracledb` module.
- * For example, `binds.name = { dir: '${BIND_OUT}', type: '${STRING}', maxSize: 40 }` will be interpolated as
- * `binds.name = { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 40 }`.
- * @typedef {SQLERExecOptions} OracleExecOptions
- * @property {Object} [driverOptions] The `oracledb` module specific options.
- * @property {Object} [driverOptions.pool] The pool attribute options passed into `oracledbPool.getConnection()`.
- * When a value is a string surrounded by `${}`, it will be assumed to be a _constant_ property that resides on the `oracledb` module and will be interpolated
- * accordingly.
+ * Oracle specific extension of the {@link typedefs.SQLERConnectionOptions} from the [`sqler`](https://ugate.github.io/sqler/) module.
+ * @typedef {Object} OracleConnectionOptionsType
+ * @property {OracleDriverOptions} [driverOptions] The `oracledb` module specific options.
+ */
+
+/**
+ * @typedef {typedefs.SQLERConnectionOptions | OracleConnectionOptionsType} OracleConnectionOptions
+ */
+
+/**
+ * Oracle specific extension of the execution options
+ * @typedef {Object} OracleExecDriverOptions
+ * @property {DBDriver.Pool} [pool] The pool attribute options passed into `oracledbPool.getConnection()`. When a value is a string surrounded by `${}`, it will be assumed
+ * to be a _constant_ property that resides on the `oracledb` module and will be interpolated accordingly.
  * For example `driverOptions.pool.someProp = '${ORACLEDB_CONSTANT}'` will be interpolated as `pool.someProp = oracledb.ORACLEDB_CONSTANT`.
- * @property {Object} [driverOptions.exec] The execution options passed into `oracledbConnection.execute()`.
- * __NOTE: `driverOptions.autoCommit` is ignored in favor of the universal `autoCommit` set directly on the {@link SQLERExecOptions}.__
+ * @property {DBDriver.ExecuteOptions} [exec] The execution options passed into `oracledbConnection.execute()`.
+ * __NOTE: `driverOptions.autoCommit` is ignored in favor of the universal `autoCommit` set directly on the {@link typedefs.SQLERExecOptions}.__
  * When a value is a string surrounded by `${}`, it will be assumed to be a _constant_ property that resides on the `oracledb` module and will be interpolated
  * accordingly.
  * For example `driverOptions.exec.someProp = '${ORACLEDB_CONSTANT}'` will be interpolated as `oracledbExecOpts.someProp = oracledb.ORACLEDB_CONSTANT`.
  */
 
 /**
+ * Oracle specific extension of the {@link typedefs.SQLERExecOptions} from the [`sqler`](https://ugate.github.io/sqler/) module. When a property of `binds` contains
+ * an object it will be _interpolated_ for property values on the `oracledb` module.
+ * For example, `binds.name = { dir: '${BIND_OUT}', type: '${STRING}', maxSize: 40 }` will be interpolated as
+ * `binds.name = { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 40 }`.
+ * @typedef {Object} OracleExecOptionsType
+ * @property {OracleExecDriverOptions} [driverOptions] The `oracledb` module specific execution options.
+ */
+
+/**
+ * @typedef {typedefs.SQLERExecOptions | OracleExecOptionsType} OracleExecOptions
+ */
+
+/**
  * Transactions are wrapped in a parent transaction object so private properties can be added (e.g. prepared statements)
  * @typedef {Object} OracleTransactionObject
- * @property {SQLERTransaction} tx The transaction
- * @property {Object} conn The connection
- * @property {Map} unprepares Map of prepared statement names (key) and no-argument _async_ functions that will be called as a pre-operation call prior to
- * `commit` or `rollback` (value)
+ * @property {typedefs.SQLERTransaction} tx The transaction
+ * @property {DBDriver.Connection} conn The connection
+ * @property {Map<String, Function>} unprepares Map of prepared statement names (key) and no-argument _async_ functions that will be called as a pre-operation
+ * call prior to `commit` or `rollback` (value)
+ */
+
+// ========================================== Internal Use ==========================================
+
+/**
+ * Internal database use
+ * @typedef {Object} InternalOracleDB
+ * @property {OracleDialect} this The dialect instance
+ * @property {Object} at The internal dialect state
+ * @property {typedefs.SQLERTrack} at.track The track
+ * @property {DBDriver} at.driver The dialect driver
+ * @property {Map<String, OracleTransactionObject>} at.transactions The transactions map
+ * @property {Object} at.pool The connection pool
+ * @property {DBDriver.PoolAttributes} at.pool.oracleConf The `oracledb` pool attributes
+ * @property {String} at.pool.alias The alias for the pool used to lookup the pool
+ * @property {DBDriver.PoolAttributes} at.pool.conf The original/raw pool attributes
+ * @property {typedefs.SQLERState} at.state The __global__ dialect state
+ * @property {Function} [at.errorLogger] A function that takes one or more arguments and logs the results as an error (similar to `console.error`)
+ * @property {Function} [at.logger] A function that takes one or more arguments and logs the results (similar to `console.log`)
+ * @property {Boolean} [at.debug] A flag that indicates the dialect should be run in debug mode (if supported)
+ * @private
  */
